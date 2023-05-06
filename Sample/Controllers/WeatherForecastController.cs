4fa924e6b9b3c2f8b409713f86ef94fa602375ad
InbornLee <vegebird@hotmail.com>
2023-05-06 20:52:01 +0800

    add area Weather

--- Sample/Controllers/WeatherForecastController.cs
+++ Sample/Controllers/WeatherForecastController.cs
@@ -1,7 +1,12 @@
+using AmSoul.Core.Extensions;
+using AmSoul.Core.Interfaces;
+using AmSoul.Core.Models;
 using AmSoul.Identity.MongoDB.Controllers;
 using AmSoul.Identity.MongoDB.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using Sample.Models;
+using System.Net.Http.Headers;
 
 namespace Sample.Controllers;
 
@@ -9,25 +14,48 @@ namespace Sample.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : BaseController
 {
-    private static readonly string[] Summaries = new[]
-    {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
     private readonly ILogger<WeatherForecastController> _logger;
     private new readonly IUserService _userService;
-    public WeatherForecastController(IUserService userService, ILogger<WeatherForecastController> logger) : base(userService)
+    private readonly IMongoCollection<AreaCode> _collection;
+
+    public WeatherForecastController(IUserService userService, ILogger<WeatherForecastController> logger, MongoDbDatabaseSetting settings) : base(userService)
     {
         _userService = userService;
         _logger = logger;
+        _collection = MongoDbQueryExtensions.GetCollection<AreaCode>(settings, $"{nameof(AreaCode).ToCamelCase()}s");
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    [HttpGet]
+    public async Task<IActionResult> GetWeather(string code)
     {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    })
-        .ToArray();
+        using var client = new HttpClient();
+        client.BaseAddress = new Uri("https://restapi.amap.com");
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+        //…Ë÷√accept±ÍÕ∑
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var cityCode = code.Trim();
+        long number1 = 0;
+        bool canConvert = long.TryParse(code, out number1);
+        if (!canConvert)
+            cityCode = (await GetCityCode(code.Trim())).AdCode;
+        var url = $"v3/weather/weatherInfo?key=7f0497b2bc9520a70736095af818d028&city={cityCode}&extensions=all";
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        return Ok(result);
+    }
+    [HttpGet("GetCityCode")]
+    public async Task<AreaCode> GetCityCode(string name)
+    {
+        var result = await _collection.FindAsync(x => x.Name.Contains(name));
+        return result.FirstOrDefault();
+    }
+    //private string GetCityCode(string cityName)
+    //{
+    //    using var db = new LiteDatabase(@"cityCodes.db");
+    //    var collection = db.GetCollection<Citycode>("cityCodes");
+    //    var result = collection.FindOne(x => x.Name.Contains(cityName));
+    //    return result.AdCode;
+    //}
+
 }
---
+++ Sample/Models/AreaCode.cs
@@ -0,0 +1,16 @@
+﻿using AmSoul.Core.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Sample.Models
+{
+    public class AreaCode : IDataModel
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? AdCode { get; set; }
+        public string? PCode { get; set; }
+        public string? Level { get; set; }
+    }
+}
